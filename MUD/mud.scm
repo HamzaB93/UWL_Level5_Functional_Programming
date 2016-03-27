a774a2865652a443ab9a63d65cc533bb294305de
unknown <hamzabhatti93@gmail.com>
2016-03-27 18:25:46 +0100

    Assignment 2: Using week 7 code, needs comments and an actual dungeon.

--- MUD/mud.scm
+++ MUD/mud.scm
@@ -4,102 +4,100 @@
 (require srfi/13)
 (require srfi/48)
 
-;; The first version of MUD game
-;; First version is basic command line
-
-; Dungeon game where player navigates rooms and tries to reach an exit
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;  Association tables
-
-; Maps a room id to a description
-(define descriptions '((1 "You are in the lobby you can see an exit to the North.")
-                       (2 "You are in the hallway there is an exit to the South") ))
-
-; Structured data, Where the player can go depending on the current room
-(define directions '((1 (north 2) (south 0) (east 0) (west 0))
-                     (2 (north 0) (south 1) (east 0) (west 0))))
+;; MUD Version 3
+;; Has advanced command line and actions
+;; NEEDS COMMENTS ON CODE
+(define descriptions '((1 "You are in the lobby")
+                       (2 "You are in the hallway")
+                       (3 "You are in a swamp")))
 
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(define look '(((directions) look) ((look) look) ((examine room) look)))
+(define quit '(((exit game) quit) ((quit game) quit) ((exit) quit) ((quit) quit)))
+; Get put into another list
+(define actions `(,@look ,@quit))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Retrieving data
 
-; Retrieve data from a association table
-; the table and the room id are arguments
-; assq will return the list that contains the given id
-; cdrs the assq to return the sentence
+(define decisiontable `((1 ((north) 2) ((north west) 3) ,@actions)
+                        (2 ((south) 1) ,@actions)
+                        (3 ,@actions)))
+
+
+(define (slist->string l)
+  (string-join (map symbol->string l)))
+
+(define (get-directions id)
+  (let ((record (assq id decisiontable)))
+    (let* ((result (filter (lambda (n) (number? (second n))) (cdr record)))
+           (n (length result)))
+      (cond ((= 0 n)
+             (printf "You appear to have entered a room with no exits.\n"))
+            ((= 1 n)
+             (printf "You can see an exit to the ~a.\n" (slist->string (caar result))))
+            (else
+             (let* ((losym (map (lambda (x) (car x)) result))
+                    (lostr (map (lambda (x) (slist->string x)) losym)))
+               (printf "You can see exits to the ~a.\n" (string-join lostr " and "))))))))
+
+
 (define (assq-ref assqlist id)
   (cdr (assq id assqlist)))
 
 (define (assv-ref assqlist id)
   (cdr (assv id assqlist)))
 
-; Get the room description by taking the room id
-; uses the descriptions table and the rid
-; uses assq-ref function to return the appropriate list depending on the id
-; cars the list to look presentable
-(define (get-room-description rid)
-  (car (assq-ref descriptions rid)))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Keyword based input
+(define (get-response id)
+  (car (assq-ref descriptions id)))
 
 (define (get-keywords id)
-  (let ((keys (assq-ref directions id)))
+  (let ((keys (assq-ref decisiontable id)))
     (map (lambda (key) (car key)) keys)))
 
+
+;; outputs a list in the form: (0 0 0 2 0 0)
 (define (list-of-lengths keylist tokens)
-  (map
+  (map 
    (lambda (x)
      (let ((set (lset-intersection eq? tokens x)))
+       ;; apply some weighting to the result
        (* (/ (length set) (length x)) (length set))))
    keylist))
 
 (define (index-of-largest-number list-of-numbers)
   (let ((n (car (sort list-of-numbers >))))
     (if (zero? n)
-        #f
-        (list-index (lambda (x) (eq? x n)) list-of-numbers))))
+      #f
+      (list-index (lambda (x) (eq? x n)) list-of-numbers))))
 
 
-; Gets the id of the room that the user wants to go to
-; Uses the current room and the atom the user inputs
-
-; First assq-ref is used to return the list of directions that the user
-; can go from the current room.
-; assq-ref used again to return the the room id by checking to see if the
-; input matches any direction from the returned list
-; lastly the room id is retunred of the new room
 (define (lookup id tokens)
-  (let* ((record (assv-ref directions id))
+  (let* ((record (assv-ref decisiontable id))
          (keylist (get-keywords id))
          (index (index-of-largest-number (list-of-lengths keylist tokens))))
-    (if index
-        (cadr (list-ref record index))
-        #f)))
+    (if index 
+      (cadr (list-ref record index))
+      #f)))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; The game
-
-; Game takes an initial id, being room 1
 (define (startgame initial-id)
-  (let loop ((id initial-id))
-    (format #t "~a\n> " (get-room-description id))
+  (let loop ((id initial-id) (description #t))
+    (if description
+        (printf "~a\n> " (get-response id))
+        (printf "> "))
     (let* ((input (read-line))
            (string-tokens (string-tokenize input))
            (tokens (map string->symbol string-tokens)))
-    (let ((response (lookup id tokens)))
-      (cond ((number? response)
-             (loop response #t))
-            ((eq? #f response)
-             (format #t "Huh? I didn't understand that!\n")
-             (loop id #f))
-            ((eq? response 'quit)
-             (format #t "BYE\n")
-             (exit)))))))
+      (let ((response (lookup id tokens)))
+        (cond ((number? response)
+               (loop response #t))
+              ((eq? #f response)
+               (format #t "huh? I didn't understand that!\n")
+               (loop id #f))
+              ((eq? response 'look)
+               (get-directions id)
+               (loop id #f))
+              ((eq? response 'quit)
+               (format #t "So Long, and Thanks for All the Fish...\n")
+               (exit)))))))
 
 (startgame 1)
